13444ad2f338da8fb6246841e771e36b7e5a3c17
Payalord <payalord@gmail.com>
2023-04-07 16:07:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Vite Plugin Pug I18n
+
+A vite plugin with support of rendering multiple pages based on pugjs templates. Plugin also support i18n with proper directory route structure creation.
+
+## Installation
+```bash
+npm i -D vite-plugin-pug-i18n
+```
+
+## Plugin Options
+Plugin Options is an object that expect's next properties:
+* pages - pages options
+    * baseDir - resolved path to pages directory
+* langs - (Optional) language options
+    * baseDir - resolved path to langs directory
+    * translate - a init function to provide translate function back. As an input this function will receive lang code
+    * fallbackLng - i18next fallback language option
+* locals - pug locals
+* options - pug options
+
+Note: `__` and `lang` are reserved locals. First is translation function that will be provided to pug, second is current language code. This locals are not provided if `langs` option are null or undefined.
+
+## Usage example
+
+Let's assume you have your pug pages files in `src/pages` directory of your project. While language `.json` files are located in `src/language`. With 2 language available there - `en` and `fr`. Then you'll need to provide next options to the plugin:
+
+```javascript
+// vite.config.js
+
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vitePluginPugI18n from 'vite-plugin-pug-i18n'
+
+export default defineConfig({
+    plugins: [
+        vitePluginPugI18n({
+            pages: {
+                baseDir: resolve(__dirname, 'src/pages')
+            },
+            langs: {
+                baseDir: resolve(__dirname, 'src/language')
+            },
+            locals: {},
+            options: {},
+        })
+    ]
+})
+```
+
+That will generate next static html files structure on build for you in `dist`:
+```bash
+dist/en/index.html
+dist/en/test-route/test-page.html
+dist/fr/index.html
+dist/fr/test-route/test-page.html
+```
+
+In case if `langs` option will not be provided or will be `null`, then static html pages will be generated without language support:
+```bash
+dist/index.html
+dist/test-route/test-page.html
+```
+
+You still can manually provide `locals` to pug with or without `langs` option. But keep in mind that plugin reserved translation function name `__`(two underscores) in pug locals (that is how proper translation function is passed to pug), which can be overriden by your locals. Same true for reserved propert name `lang`.
+
+## i18next
+
+Plugin is using i18next as a translation function. Language filename must have next format `[language-code].json`, and the file structure is:
+```json
+// en.json
+{
+    "hello": "Hello!"
+}
+```
+
+So it is key/value json format. So in pug template you can use it like this:
+```pug
+// index.pug
+p #{__('hello')}
+```
+
+## Javascript files
+
+You can use `*.js` files in pug that will be processed as vite assets and properly replaced, example:
+```pug
+// index.pug
+p #{__('hello')}
+script(type='module', src='src/main.js')
+```
+
+Where `main.js` is normal ESMAScript that will be compiled by vite as asset. Within which you can import any `sass`, `scss`, `css` files.
---
+++ src/index.ts
@@ -0,0 +1,234 @@
+import path from "path"
+import fs from "fs"
+import pug from "pug"
+import i18next from "i18next"
+
+/**
+ * Pages options
+ * @param baseDir string
+ */
+interface PagesOptions {
+    baseDir: string
+}
+
+type Translate = (langCode: string) => (a: string) => string
+type FallbackLng = string | Array<string> | object
+
+/**
+ * Language options
+ * @param baseDir string
+ */
+interface LangsOptions {
+    baseDir: string,
+    fallbackLng: FallbackLng | ((code: string) => FallbackLng) | false | undefined,
+    translate: Translate
+}
+
+/**
+ * Plugin options
+ * @param pages PagesOptions
+ * @param langs LangsOptions
+ * @param locals object
+ * @param options object
+ */
+interface PluginOptions {
+    pages: PagesOptions,
+    langs: LangsOptions | undefined,
+    locals: object | undefined,
+    options: pug.Options | undefined
+}
+/**
+ * A page interface to store lang code and page path for meta map data
+ * @param langCode string
+ * @param page string
+ */
+interface MetaPage {
+    langCode: string | null,
+    page: string
+}
+
+/**
+ * Check if provided path is directory
+ * @param path string
+ * @returns Promise<boolean>
+ */
+const isDirectory = async (path: string): Promise<boolean> => {
+    const stats = await fs.promises.stat(path)
+    return stats.isDirectory()
+}
+
+/**
+ * Scans directory and sub-directories recursively for files with provided extension and returns it as an array
+ * @param baseDir string
+ * @param ext string (optional)
+ * @returns Promise<Array<string>>
+ */
+const getFilelist = async (baseDir: string, ext = '.pug'): Promise<Array<string>> => {
+    const files = await fs.promises.readdir(baseDir)
+    return files
+        .reduce(async (p: Promise<Array<string>> | Array<string>, i): Promise<Array<string>> => {
+            p = await p
+            const resolvedPath = path.resolve(baseDir, i)
+            if (await isDirectory(resolvedPath)) {
+                const filelist = await getFilelist(resolvedPath)
+                p = p.concat(filelist)
+            } else if (path.extname(resolvedPath) === ext) {
+                p.push(resolvedPath)
+            }
+            return p
+        }, [])
+}
+
+/**
+ * A vite plugin to compile pugjs templates as static pages with language route and translation support
+ * @param options PluginOptions
+ * @returns object
+ */
+const vitePluginPugI18n = function ({pages, langs, locals, options}: PluginOptions) {
+    const langMap = new Map<string, string>()
+    const langMetaMap = new Map<string, MetaPage>()
+    const pageMap = new Map<string, pug.compileTemplate>()
+    let translate: (a: string) => string
+    let langsFound: Array<string> = []
+    let pagesFound: Array<string> = []
+
+    let root = ''
+
+    const loadLang = async (lang: string) => {
+        const langCode = path.basename(lang, ".json")
+        const langJson = await fs.promises.readFile(lang, "utf-8")
+        langMap.set(langCode, JSON.parse(langJson))
+    }
+    
+    const loadLangs = async (langs: LangsOptions) => {
+        langsFound = await getFilelist(langs.baseDir, '.json')
+        await Promise.all(langsFound.map(loadLang))
+    }
+
+    const loadPages = async () => {
+        pagesFound = await getFilelist(pages.baseDir)
+    }
+
+    const processPages = () => {
+        const input = {}
+        // inject entry files here
+        for (const page of pagesFound) {
+            if (langs) {
+                for (const langCode of langMap.keys()) {
+                    const relativePath = path
+                        .relative(pages.baseDir, page)
+                        .replace(/\.pug$/, ".html")
+                    const distPath = path.normalize(`${langCode}/${relativePath}`)
+                    input[distPath] = distPath
+                    langMetaMap.set(distPath, {
+                        langCode,
+                        page
+                    })
+                }
+            } else {
+                const distPath = path
+                    .relative(pages.baseDir, page)
+                    .replace(/\.pug$/, ".html")
+                input[distPath] = distPath
+                langMetaMap.set(distPath, {
+                    langCode: null,
+                    page
+                })
+            }
+        }
+        return input
+    }
+
+    return {
+        name: "vite-plugin-pug-i18n",
+        enforce: "pre",
+        apply: "build",
+
+        async config() {
+            if (langs) {
+                await Promise.all([loadPages(), loadLangs(langs)])
+            } else {
+                await loadPages()
+            }
+
+            return {
+                build: {
+                    rollupOptions: {
+                        input: processPages()
+                    }
+                }
+            }
+        },
+
+        // Save root on configResolved
+        configResolved(resolvedConfig) {
+            root = resolvedConfig.root
+        },
+
+        // Init I18n on buildStart
+        buildStart() {
+            if (langs) {
+                const resources = {}
+                for (const [langCode, langObject] of langMap.entries()) {
+                    resources[langCode] = {
+                        translation: langObject
+                    }
+                }
+            
+                i18next.init({
+                    fallbackLng: langs.fallbackLng || langMap.keys()[0],
+                    supportedLngs: [...langMap.keys()],
+                    resources,
+                })    
+            }
+        },
+
+        resolveId(id, importer) {
+            // Resolve html files generated by pug
+            if (langMetaMap.has(id)) {
+                return id
+            }
+            // Resolve static assets in pug files
+            if (importer && langMetaMap.has(importer)) {
+                if (id === "vite/modulepreload-polyfill") {
+                    // Ignore vite modulepreload polyfill
+                    return
+                }
+                return path.resolve(root, id)
+            }
+        },
+
+        // Transform pug to html
+        async load(id) {
+            const meta = langMetaMap.get(id)
+            if (!meta) {
+                return
+            }
+            const { langCode, page } = meta
+
+            let template = pageMap.get(page)
+            if (!template) {
+                template = pug.compileFile(page, options)
+                pageMap.set(page, template)
+            }
+
+            if (langs && langCode) {
+                if (typeof langs.translate === 'function') {
+                    translate = langs.translate(langCode)
+                } else {
+                    translate = i18next.getFixedT(langCode)
+                }
+    
+                return template({
+                    __: translate,
+                    lang: langCode,
+                    ...locals,
+                })
+            } else {
+                return template(locals || undefined)
+            }
+        }
+    }
+}
+
+export default vitePluginPugI18n
